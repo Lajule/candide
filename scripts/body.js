d0f8b1566ebc1a9fefe95d1a276b57b7c2f9e1f5
Lajule <julien.rouzieres@mac.com>
2018-05-12 20:10:14 +0200

    more look

--- scripts/body.js
+++ scripts/body.js
@@ -5,13 +5,14 @@ body.$inject = ["$scope", "$timeout", "$sce", "$filter", "resume"];
 export function body($scope, $timeout, $sce, $filter, resume) {
   let vm = this;
 
+  vm.step = 0;
   vm.preview = true;
   vm.resume = resume;
 
   vm.reset = () => {
     resume.name = "";
     resume.title = "";
-    resume.skills = "";
+    resume.skills = "<div></div>";
     resume.degrees = [];
     vm.addDegree();
     resume.experiences = [];
@@ -33,7 +34,7 @@ export function body($scope, $timeout, $sce, $filter, resume) {
       from: "",
       to: "",
       description: "",
-      mission: ""
+      mission: "<div></div>"
     });
   };
 
@@ -54,38 +55,49 @@ export function body($scope, $timeout, $sce, $filter, resume) {
     "ctrl.resume",
     (newValue, oldValue) => {
       let pdf = new jsPDF("portrait", "mm", "a4");
-      let height = 15;
 
       pdf.setProperties({
         title: $filter("title")(resume, ".pdf")
       });
 
-      pdf.text(15, height, resume.name);
-      height += 15;
+      pdf.text(15, 15, resume.name);
+      pdf.text(15, 30, resume.title);
+      pdf.fromHTML(resume.skills, 15, 45, { width: 155 });
+      pdf.addPage();
 
-      pdf.text(15, height, resume.title);
-      height += 15;
+      let height = 15;
 
-      pdf.fromHTML(resume.skills, 15, height, { width: 170 });
-      height += 50;
+      pdf.text(15, height, "Formation");
+      height += 15;
 
       for (let i = 0; i < resume.degrees.length; ++i) {
         let degree = resume.degrees[i];
+
+        if (height >= 295) {
+          pdf.addPage();
+          height = 15;
+        }
 
         pdf.text(15, height, `${degree.school} - ${degree.year}`);
         height += 15;
 
         pdf.text(15, height, degree.name);
         height += 15;
+      }
+      pdf.addPage();
+
+      height = 15;
+ 
+      pdf.text(15, height, "Epériences");
+      height += 15;
+
+      for (let i = 0; i < resume.experiences.length; ++i) {
+        let experience = resume.experiences[i];
 
         if (height >= 295) {
           pdf.addPage();
           height = 15;
         }
-      }
-
-      for (let i = 0; i < resume.experiences.length; ++i) {
-        let experience = resume.experiences[i];
 
         pdf.text(15, height, `${experience.firm} - ${experience.client}`);
         height += 15;
@@ -96,14 +108,10 @@ export function body($scope, $timeout, $sce, $filter, resume) {
         pdf.text(15, height, experience.description);
         height += 15;
 
-        pdf.fromHTML(experience.mission, 15, height, { width: 170 });
-        height += 50;
-
-        if (height >= 295) {
-          pdf.addPage();
-          height = 15;
-        }
+        pdf.fromHTML(experience.mission, 15, height, { width: 155 });
+        height += 150;
       }
+      pdf.addPage();
 
       vm.source = $filter("json")(resume);
       vm.pdf = $sce.trustAsResourceUrl(pdf.output("bloburi"));
