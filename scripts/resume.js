22db60d08d902e16b807c4830429fcf32edc5154
Julien Rouzieres <julien.rouzieres@mac.com>
2018-05-12 16:57:47 +0200

    look

--- scripts/resume.js
+++ scripts/resume.js
@@ -2,26 +2,26 @@ resume.$inject = [];
 
 export function resume() {
   return {
-    name: "Harry P.",
-    title: "Magician",
+    name: "",
+    title: "",
     skills:
-      "<p>Miscellaneous spells</p>",
+      "<div></div>",
     degrees: [
       {
-        school: "Hogwarts School of Witchcraft and Wizardry",
-        year: "1998",
-        name: "Gryffindor"
+        school: "",
+        year: "",
+        name: ""
       }
     ],
     experiences: [
       {
-        firm: "Capgemini",
-        client: "Bouygues Telecom",
-        from: "2004-05",
-        to: "2018-04",
-        description: "Membre de l'équipe SI Entreprise",
+        firm: "",
+        client: "",
+        from: "",
+        to: "",
+        description: "",
         mission:
-          "<p>Kill spells</p>"
+          "<div></div>"
       }
     ]
   };
