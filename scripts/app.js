1cad6cd0a0a54d76f1b0c682154f634da4cfec63
ROUZIERES Julien (Prestataire) <Julien.ROUZIERES@cpexterne.org>
2018-05-14 14:45:21 +0200

    add lang

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,7 @@ import { pdf } from "./pdf";
 import { picker } from "./picker";
 import { wysiwyg } from "./wysiwyg";
 import { code } from "./code";
+import { en, fr } from "./lang";
 
 angular
   .module("app", ["pascalprecht.translate"])
@@ -25,38 +26,10 @@ angular
     "$translateProvider",
     ($compileProvider, $translateProvider) => {
       $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|blob):/);
-
       $translateProvider
-        .translations("en", {
-          subtitle: "Generate your PDF resume",
-          new: {
-            label: "New",
-            tooltip: "Clear all data !"
-          },
-          save: {
-            label: "Save",
-            tooltip: "Save data to a file"
-          },
-          download: {
-            label: "Download",
-            tooltip: "Download your PDF resume"
-          }
-        })
-        .translations("fr", {
-          subtitle: "Générez votre CV au format PDF",
-          new: {
-            label: "Nouveau",
-            tooltip: "Tout effacer !"
-          },
-          save: {
-            label: "Enregistrer",
-            tooltip: "Sauvegarde les données dans un fichier"
-          },
-          download: {
-            label: "Télécharger",
-            tooltip: "Récupére votre CV en pdf"
-          }
-        })
+        .translations("en", en)
+        .translations("fr", fr)
+        .useSanitizeValueStrategy("sce")
         .preferredLanguage("fr");
     }
   ]);
---
+++ scripts/lang.js
@@ -0,0 +1,31 @@
+export let en = {
+  subtitle: "Generate your PDF resume",
+  new: {
+    label: "New",
+    tooltip: "Clear all data !"
+  },
+  save: {
+    label: "Save",
+    tooltip: "Save data to a file"
+  },
+  download: {
+    label: "Download",
+    tooltip: "Download your PDF resume"
+  }
+};
+
+export let fr = {
+  subtitle: "Générez votre CV au format PDF",
+  new: {
+    label: "Nouveau",
+    tooltip: "Tout effacer !"
+  },
+  save: {
+    label: "Enregistrer",
+    tooltip: "Sauvegarde les données dans un fichier"
+  },
+  download: {
+    label: "Télécharger",
+    tooltip: "Récupére votre CV en pdf"
+  }
+};
