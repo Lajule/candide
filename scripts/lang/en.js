53218b9f20a92655df3ebae3ee02d166b7d9c948
ROUZIERES Julien (Prestataire) <Julien.ROUZIERES@cpexterne.org>
2018-06-11 16:44:35 +0200

    lang directory

---
+++ scripts/lang/en.js
@@ -0,0 +1,41 @@
+export let en = {
+    subtitle: "Generate your PDF resume",
+    new: {
+      label: "New",
+      tooltip: "Clear all data !"
+    },
+    save: {
+      label: "Save",
+      tooltip: "Save data to a file"
+    },
+    download: {
+      label: "Download",
+      tooltip: "Download your PDF resume"
+    },
+    people: "People",
+    education: "Education",
+    experiences: "Experiences",
+    name: "Name",
+    title: "Tilte",
+    skills: "Skills",
+    school: "School",
+    year: "Year",
+    degree: "Degree",
+    firm: "Firm",
+    client: "Client",
+    from: "From",
+    to: "To",
+    description: "Description",
+    mission: "Mission",
+    add: {
+      school: "Add a school",
+      experience: "Add an experience"
+    },
+    remove: {
+      school: "Remove school",
+      experience: "Remove experience"
+    },
+    source: "Source",
+    pdf: "PDF"
+  };
+  
---
+++ scripts/lang/fr.js
@@ -0,0 +1,41 @@
+export let fr = {
+    subtitle: "Générez votre CV au format PDF",
+    new: {
+      label: "Nouveau",
+      tooltip: "Tout effacer !"
+    },
+    save: {
+      label: "Enregistrer",
+      tooltip: "Sauvegarde les données dans un fichier"
+    },
+    download: {
+      label: "Télécharger",
+      tooltip: "Récupére votre CV en pdf"
+    },
+    people: "Personne",
+    education: "Formation",
+    experiences: "Expériences",
+    name: "Nom",
+    title: "Titre",
+    skills: "Compétences",
+    school: "Ecole",
+    year: "Année",
+    degree: "Diplôme",
+    firm: "Société",
+    client: "Client",
+    from: "De",
+    to: "A",
+    description: "Description",
+    mission: "Mission",
+    add: {
+      school: "Ajouter une école",
+      experience: "Ajouter une expérience"
+    },
+    remove: {
+      school: "Supprimer école",
+      experience: "Supprimer expérience"
+    },
+    source: "Source",
+    pdf: "PDF"
+  };
+  
